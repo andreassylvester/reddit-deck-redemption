43e2e3c847de9aabc497a1945ea237d617db8a45
Andreas Palmerén <andreas.palmeren@gmail.com>
2024-04-13 13:44:34 +0200

    don't bother with eslint for this project

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { EsLinter, linterPlugin, TypeScriptLinter } from "vite-plugin-linter"
 
 // https://vitejs.dev/config/
 export default defineConfig((configEnv) => ({
-  plugins: [vue(),
-  linterPlugin({
-    include: ["./src/**/*.ts", "./src/**/*.tsx"],
-    linters: [new EsLinter({ configEnv: configEnv }), new TypeScriptLinter()],
-  }),],
+  plugins: [vue()],
 }))
